be37312a883f5efcc3f1f5fa0ecd438b0a3fe35b
Pranjal Naphade <naphadepranjal@gmail.com>
2023-07-06 16:24:06 +0530

    Job added

--- myapp/api/jobs.py
+++ myapp/api/jobs.py
@@ -4,7 +4,8 @@ from flask.views import MethodView
 from flask_smorest import Blueprint
 from myapp.response import APIResponse
 from myapp.data_schema.schema import *
-
+from uuid import uuid4
+import datetime
 jobs_blueprint = Blueprint("jobs", __name__)
 @jobs_blueprint.route('/jobs')
 class JobResource(MethodView):
@@ -12,7 +13,7 @@ class JobResource(MethodView):
         request_data = request.get_json()
         default_value = None
         job_data = {
-            "_id": request_data.get("_id"),
+            "_id": uuid4().hex,
             "title": request_data.get("title"),
             "recruiter": request_data.get("recruiter", default_value),
             "description": request_data.get("description", default_value),
@@ -23,14 +24,12 @@ class JobResource(MethodView):
             "experience": request_data.get("experience", default_value),
             "language": request_data.get("language", default_value),
             "extra": request_data.get("extra", default_value),
-            "created_at": request_data.get("created_at", default_value),
+            "created_at": datetime.datetime.now(),
             "created_by": request_data.get("created_by", default_value),
-            "updated_at": request_data.get("updated_at", default_value),
             "is_deleted": 0
         }
-
         job = Job(**job_data)
         job.save()
         job_id = job._id
         job = job.to_mongo().to_dict()
-        return APIResponse.respond(job, "Job created Successfully!", 200)
+        return APIResponse.respond(job, f"{job_id} Job created Successfully!", 200)
